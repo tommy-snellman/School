b733a067cdb23cb7059025e1c8b5601afef7fb32
mathetic <mathetic@users.noreply.github.com>
2016-03-09 16:14:34 +0200

    Added files via upload

--- GraphPanel.java
+++ GraphPanel.java
@@ -1,11 +1,10 @@
 package view;
+import java.awt.Color;
+import java.awt.Graphics;
+
 import javax.swing.JPanel;
 
 import model.Stock;
-
-import java.awt.Color;
-import java.awt.Graphics;
-import java.util.ArrayList;
 
 class GraphPanel extends JPanel {
 	
@@ -15,17 +14,22 @@ class GraphPanel extends JPanel {
 		super.paintComponent(g);
 		
 		int width = this.getWidth();
-		double s1Min = stock1.getMin();
-		double s1Max = stock1.getMax();
-		double s2Min = stock2.getMin();
-		double s2Max = stock2.getMax();
-		
-		g.setColor(Color.red);
-		int size = stock1.getSize();
-		for (int i = 0; i < size - 1; i++){
-			g.drawLine( map(i, 0, size, 0, width), (int) norm(s1Min, s1Max,stock1.getCloneClose(i)), map(i+1, 0, size, 0, width), (int) norm(s1Min, s1Max,  stock1.getCloneClose(i + 1)));
+		System.out.println("Hej");
+		if (stock1 != null && stock2 != null){
+			System.out.println("Hej igen");
+			double s1Min = stock1.getMin();
+			double s1Max = stock1.getMax();
+			double s2Min = stock2.getMin();
+			double s2Max = stock2.getMax();
+			
+			g.setColor(Color.red);
+			int size = stock1.getSize();
+			System.out.println(map(0, 0, size, 0, width));
+			for (int i = 0; i < size - 1; i++){
+				g.drawLine(map(i, 0, size, 0, width), (int) norm(s1Min, s1Max,stock1.getCloneClose(i)), map(i+1, 0, size, 0, width), (int) norm(s1Min, s1Max,  stock1.getCloneClose(i + 1)));
+			}
+			
 		}
-		
 	}
 	
 	public double norm( double Min, double Max, double value) {
